a7acea2a15d7513c45d3eb4fae1a969f486dca9a
SteppenLion <marek.kison@gmail.com>
2021-11-10 22:02:36 +0100

    small change

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- IF PACKER IS not installed
+local fn = vim.fn
+local cmd = vim.cmd
+local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+
 require('colorscheme')
 require('plugins')
 require("lsp")
